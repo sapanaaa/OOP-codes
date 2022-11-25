60929935f6eee1e69e6a7b8d719a8d772efc0bcf
Sapana Pandey <sapanapandey447@gmail.com>
2022-11-25 23:02:44 +0545

    int commmit for me for oop codes

---
+++ first_file.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    system("cls"); //for clearing screen
+    int a,b,sum; //variables are decleared here
+    cout<<"Enter two numbers=";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"The sum of two numbers are"<<sum;
+}
