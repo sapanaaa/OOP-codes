f6a97d3617ad870e6300145af3aa56fcf16f6268
Sapana Pandey <sapanapandey447@gmail.com>
2022-11-25 23:31:55 +0545

    commit fourth

---
+++ third+file.cpp
@@ -0,0 +1,11 @@
+//Program for entering name and printing name 
+#include<iostream>
+using namespace std;
+int main()
+{
+    system("cls");
+    string cat;  //declearing cat string type variable
+    cout<<"Enter your name :"<<endl;
+    cin>>cat;
+    cout<<"Your name is :"<<cat<<endl;
+}
