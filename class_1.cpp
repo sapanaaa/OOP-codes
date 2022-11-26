41ea9ebac20d5f2213be359a35b9d80646ddf5c2
Sapana Pandey <sapanapandey447@gmail.com>
2022-11-26 19:17:03 +0545

    commit by me

--- class_1.cpp
+++ class_1.cpp
@@ -1,31 +1,33 @@
 #include<iostream>
-#include<conio.h>
+// #include<conio.h>  not required
 using namespace std;
 class student
 {
 private:
-    char name;
+// for names use string data type not char
+    string name;
     int roll;
     float marks;
-    void enter();
+    // void enter(); illegal garna mildaena
 public:
     int age;
     void input()
     {
         cout<<"enter name, roll no, marks and age"<<endl;
-        cin>>name>>roll>marks>>age;
+        cin>>name>>roll>>marks>>age;
     }
     void display()
     {
-        cout<<name<<roll<<marks<<age;
+        cout<<"The given info of :"<<name<<endl<<roll<<endl<<marks<<endl<<age;
     }
 };
 int main()
 {
+    system("cls");
     student ram, om;
     ram.input();
     ram.display();
-    ram.enter();
+    // ram.enter();   enter function not defined
     om.input();
     om.display();
     return 0;
