9fa4b7cedbdacc40b02bb83b05aa73115653004b
Sapana pandey <114503539+sapanaaa@users.noreply.github.com>
2023-01-08 12:27:04 +0545

    code for entering name and printing name

--- third+file.cpp
+++ third+file.cpp
@@ -1,10 +1,10 @@
-//Program for entering name and printing name 
+//code for entering name and printing name 
 #include<iostream>
 using namespace std;
 int main()
 {
     system("cls");
-    string cat;  //declearing cat string type variable
+    string cat;  //declearing string type variable
     cout<<"Enter your name :"<<endl;
     cin>>cat;
     cout<<"Your name is :"<<cat<<endl;
